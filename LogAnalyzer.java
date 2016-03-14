7926f62db7bd485d61d4bf02769db0d4277f6c62
KanverBains <bainskanver@gmail.com>
2016-03-14 10:26:33 -0400

    4.72
    4.72 Commit

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -23,6 +23,11 @@ public class LogAnalyzer
         // Create the reader to obtain the data.
         reader = new LogfileReader();
     }
+    
+    public LogAnalyzer(String fileName){
+        hourCounts = new int[24];
+        reader = new LogfileReader(fileName);
+    }
 
     /**
      * Analyze the hourly access data from the log file.
